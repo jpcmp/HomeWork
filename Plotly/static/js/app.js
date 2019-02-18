feb0bfc60c2f9b2ca94e3e8d063294c51430eeb7
jpcmp <joaopaulocampos@gmail.com>
2019-02-17 23:57:26 -0500

    up

--- Plotly/static/js/app.js
+++ Plotly/static/js/app.js
@@ -3,20 +3,17 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-  d3.json("/metadata/" + sample, function(error, response){
-    if (error) return console.warn(error);
-    var resKeys = Object.keys(response);
-    console.log(resKeys)
+  var meta = "/metadata/" + sample;
+  d3.json(meta).then(function(sample){
     // Use d3 to select the panel with id of `#sample-metadata`
-    var panel = d3.select("#sample-metadata");
+    var sample_meta = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-    panel.html(""); 
+    sample_meta.html(""); 
     // Use `Object.entries` to add each key and value pair to the panel
-    for (var i=0; i<resKeys.length; i++){
-      var $newDataLine = document.createElement('p');
-      $newDataLine.innerHTML = resKeys[i] + ": " + response[resKeys[i]];
-      $panel.appendChild($newDataLine)
-    };   
+    Object.entries(sample).forEach(([key, value]) => {
+      var row = sample_meta.append("p");
+      row.text(`${key}: ${value}`);
+    })   
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
   });
@@ -27,53 +24,50 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var labels = sampleData[0]['otu_ids'].map(function(item) {
-    return otuData[item]
-  });
+  var sam = `/samples/${sample}`;
+  d3.json(sam).then(function(data){
     // @TODO: Build a Bubble Chart using the sample data
-    var bubbleLayout = {
-      margin: { t: 0 },
-      hovermode: 'closest',
-      xaxis: { title: 'OTU ID' }
-  };
-  var bubbleData = [{
-      x: sampleData[0]['otu_ids'],
-      y: sampleData[0]['sample_values'],
-      text: labels,
+    var xValues = data.otu_ids;
+    var yValues = data.sample_values;
+    var tValues = data.otu_labels;
+    var mSize = data.sample_values;
+    var mClrs = data.otu_ids;
+    var trace_bubble = {
+      x: xValues,
+      y: yValues,
+      text: tValues,
       mode: 'markers',
       marker: {
-          size: sampleData[0]['sample_values'],
-          color: sampleData[0]['otu_ids'],
-          colorscale: "Portland",
+        size: mSize,
+        color: mClrs
       }
-  }];
-  var BUBBLE = document.getElementById('bubble');
-  Plotly.plot(BUBBLE, bubbleData, bubbleLayout);
+    };
+    var data = [trace_bubble];
+    var layout = {
+      xaxis: {title: "OTU ID"}
+    };
+    Plotly.newPlot('bubble', data, layout);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-    var pieData = [{
-      values: sampleData[0]['sample_values'].slice(0, 10),
-      labels: sampleData[0]['otu_ids'].slice(0, 10),
-      hovertext: labels.slice(0, 10),
-      hoverinfo: 'hovertext',
-      type: 'pie',
-      marker: {
-          colors: ['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
-      }
-    }];
-
-    var pieLayout = {
-      margin: { t: 0, l: 0 }
-    };
-
-    var PIE = document.getElementById('pie');
-    Plotly.plot(PIE, pieData, pieLayout);
+    d3.json(sam).then(function(data) {
+      var pieValue = data.sample_values.slice(0,10);
+      var pielabel = data.otu_ids.slice(0, 10);
+      var pieHover = data.otu_labels.slice(0, 10);
+      var data = [{
+        values: pieValue,
+        labels: pielabel,
+        hovertext: pieHover,
+        type: 'pie'
+      }];
+      Plotly.newPlot('pie', data);
+    });
+  });
 }
 
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#sample-metadata");
+  var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
